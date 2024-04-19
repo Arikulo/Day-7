0ec36c562f751eab06c8f34ce3d56698807bba76
Arikulo <163147130+Arikulo@users.noreply.github.com>
2024-04-19 13:54:59 +0000

    Updated

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ IIVK = []
 TP = []
 OP = []
 HC = []
-
+types = [VK,IVK,FH,IIVK,TP,OP,HC]
+names = ['VK','IVK','FH','IIVK','TP','OP','HC']
 
 def get_hand_type(hand):
     unique = set(hand)
@@ -31,8 +32,33 @@ def get_hand_type(hand):
     else:
         return 'High Card'
 
+def sort_by_value(hands_list):
+ values =['A','K','Q','J','T','9','8','7','6','5','4','3','2']
+ return sorted(hands_list, key=lambda x: values.index(x[0][0]))
+
+
 for place, data in enumerate(x):
     hand = data[0]
-    value = data[1]
+    bet = data[1]
     hand_type = get_hand_type(hand)
-    print(hand, hand_type)
+  
+    if hand_type == 'Five of a Kind':
+        VK.append([hand,bet])
+    elif hand_type == 'Four of a Kind':
+        IVK.append([hand,bet])
+    elif hand_type == 'Full House':
+        FH.append([hand,bet])
+    elif hand_type == 'Three of a Kind':
+        IIVK.append([hand,bet])
+    elif hand_type == 'Two Pairs':
+        TP.append([hand,bet])
+    elif hand_type == 'One Pair':
+        OP.append([hand,bet])
+    else:
+        HC.append([hand,bet])
+
+
+for place,hand_type in enumerate(types):
+  types[place] = sort_by_value(hand_type)
+  print(names[place],':',types[place])
+
