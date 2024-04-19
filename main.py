9d3a99b7e4187e5e1ae7c4a8bd6cbd94ec16c6a0
Arikulo <163147130+Arikulo@users.noreply.github.com>
2024-04-19 13:41:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 x=[['32T3K',765],['T55J5',684],['KK677',28],['KTJJT',220],['QQQJA',483]]
-#howdy
+
+VK= []
+IVK = []
+FH = []
+IIVK = []
+TP = []
+OP = []
+HC = []
+
+
 def get_hand_type(hand):
     unique = set(hand)
     counts = []
